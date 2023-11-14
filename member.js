ca181c1c22536c00963ab7cee13d68be01e516ff
Luis Rodríguez <81327657+luis-roel@users.noreply.github.com>
2023-11-14 17:28:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()
+{
+    return {
+    restrict: 'E', templateUrl: modules/skills/views/member.html', controller: 'SkillsMemberController', controllerAs: 'vm',
+    bindToController: true,
+    scope: {
+    member: '='
+    }
+};
+}    
